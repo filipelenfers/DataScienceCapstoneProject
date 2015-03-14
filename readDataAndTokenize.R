1dfecd1646cd14f8976ed38bf92f9fc034ba7acd
Filipe Pais Lenfers <filipelenfers@gmail.com>
2015-03-14 16:04:13 -0300

    Trigram frequenci extraction.

--- readDataAndTokenize.R
+++ readDataAndTokenize.R
@@ -1,22 +1,36 @@
 library(stringi)
 library(tm)
+library(RWeka)
 
 blogs <- readLines("data/en_US/en_US.blogs.txt", encoding="UTF-8")
 twitter <- readLines("data/en_US/en_US.twitter.txt", encoding="UTF-8")
 news  <- readLines("data/en_US/en_US.news.txt", encoding="UTF-8")
 
-sample_blogs <- sample(blogs,1000)
-sample_twitter <- sample(twitter,1000)
-sample_news <- sample(news,1000)
+sample_blogs <- sample(blogs,100000)
+sample_twitter <- sample(twitter,100000)
+sample_news <- sample(news,100000)
 
 rm(blogs,twitter,news)
 
 # dirSource <- DirSource(directory='data/en_us',
 #                        encoding='utf-8')
 # corpus <- Corpus(dirSource)
-corpus <- Corpus(VectorSource(list(sample_blogs,sample_twitter,sample_news)))
+#corpus <- Corpus(VectorSource(list(sample_blogs,sample_twitter,sample_news)))
+corpus <- Corpus(VectorSource(list(sample_blogs)))
 
 corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, content_transformer(removePunctuation))
+corpus <- tm_map(corpus, content_transformer(removeNumbers))
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+corpus <- tm_map(corpus, stripWhitespace)
+#corpus <- tm_map(corpus, removeSparseTerms,0.8)
+
+TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+termdocmatrix <- TermDocumentMatrix(corpus, control = list(tokenize = TrigramTokenizer))
+
+#head(data.frame(inspect(termdocmatrix)),100)
+trigram.df <- data.frame(termdocmatrix$dimnames$Terms,termdocmatrix$v)
+
 
 #UnigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
 
