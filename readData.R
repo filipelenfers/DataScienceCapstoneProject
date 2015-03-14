7b86163da2fbc0b63d39cd30b26dc8ba9f6651da
Filipe Pais Lenfers <filipelenfers@gmail.com>
2015-03-14 14:43:11 -0300

    Project start.

---
+++ readData.R
@@ -0,0 +1,24 @@
+library(stringi)
+library(tm)
+
+blogs <- readLines("data/en_US/en_US.blogs.txt", encoding="UTF-8")
+twitter <- readLines("data/en_US/en_US.twitter.txt", encoding="UTF-8")
+news  <- readLines("data/en_US/en_US.news.txt", encoding="UTF-8")
+
+sample_blogs <- sample(blogs,1000)
+sample_twitter <- sample(twitter,1000)
+sample_news <- sample(news,1000)
+
+rm(blogs,twitter,news)
+
+#UnigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
+
+#options(mc.cores=4)
+
+#tdm1gram <- TermDocumentMatrix(corpus, control = list(tokenize = UnigramTokenizer))
+
+
+# dirSource <- DirSource(directory='data/en_us',
+#                        encoding='utf-8')
+# corpus <- Corpus(dirSource)
+# corpus <- tm_map(corpus, content_transformer(tolower))
