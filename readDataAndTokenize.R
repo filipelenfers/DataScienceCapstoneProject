3ff945ef7aa23dccd76ad990672f6511aac9ba88
Filipe Pais Lenfers <filipelenfers@gmail.com>
2015-03-14 15:18:01 -0300

    to lower transform

--- readDataAndTokenize.R
+++ readDataAndTokenize.R
@@ -11,6 +11,13 @@ sample_news <- sample(news,1000)
 
 rm(blogs,twitter,news)
 
+# dirSource <- DirSource(directory='data/en_us',
+#                        encoding='utf-8')
+# corpus <- Corpus(dirSource)
+corpus <- Corpus(VectorSource(list(sample_blogs,sample_twitter,sample_news)))
+
+corpus <- tm_map(corpus, content_transformer(tolower))
+
 #UnigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
 
 #options(mc.cores=4)
@@ -18,7 +25,4 @@ rm(blogs,twitter,news)
 #tdm1gram <- TermDocumentMatrix(corpus, control = list(tokenize = UnigramTokenizer))
 
 
-# dirSource <- DirSource(directory='data/en_us',
-#                        encoding='utf-8')
-# corpus <- Corpus(dirSource)
-# corpus <- tm_map(corpus, content_transformer(tolower))
+
