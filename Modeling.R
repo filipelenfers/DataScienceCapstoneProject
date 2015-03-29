0039b11e28a56fe2e32a360ff9e11ad23d574ffa
Filipe Pais Lenfers <filipelenfers@gmail.com>
2015-03-29 11:53:06 -0300

    Modeling tests
    Modeling tests

--- Modeling.R
+++ Modeling.R
@@ -1,68 +1,127 @@
 options( java.parameters = "-Xmx4g" )
 
-library(stringi)
 library(tm)
 library(RWeka)
-library(ggplot2)
 library(dplyr)
+library(doParallel)
+
+
+nodes <- detectCores()
+cl <- makeCluster(nodes)
+registerDoParallel(cl)
 
 source("baseFunctions.R")
 
 blog.file <- "data/en_US/en_US.blogs.txt"
 blogs <- readLines(blog.file, encoding="UTF-8")
-sample.size <- trunc(length(blogs)*0.02)
-sample.blogs <- sample(blogs,sample.size)
-rm(blogs)
+blogs <- sample(blogs,length(blogs)*0.01)
 
-train.sample.idx <- sample.int(length(sample.blogs), length(sample.blogs)*0.80)
-train.sample.blogs <- sample.blogs[train.sample.idx]
-test.sample.blogs <- sample(sample.blogs[-train.sample.idx])
+twitter.file <- "data/en_US/en_US.twitter.txt"
+tweets <- readLines(twitter.file, encoding="UTF-8")
+
+news.file <- "data/en_US/en_US.news.txt"
+news <- readLines(news.file, encoding="UTF-8")
+
+all <- c(blogs,tweets,news)
+
+rm(blogs,tweets,news)
+
+#sample.size <- trunc(length(blogs)*0.01)
+#sample.blogs <- sample(blogs,sample.size)
+#rm(blogs)
+
+train.sample.idx <- sample.int(length(all), length(all)*0.80)
+train.sample <- all[train.sample.idx]
+test.sample <- all[-train.sample.idx]
+rm(all,train.sample.idx)
+save(train.sample,file = "train.sample.RData")
+save(test.sample,file = "test.sample.RData")
+
 
 #Create clean corpus-------------------------------------------
-train.corpus.blogs <- createCorpus(train.sample.blogs)
-test.corpus.blogs <- createCorpus(test.sample.blogs)
-dummy.corpus <- createCorpus(c("<s> I am Sam </s>",
-                               "<s> Sam I am </s>",
-                               "<s> I do not like green eggs and ham </s>"),clean = F)
+train.corpus <- createCorpus(train.sample)
+rm(train.sample)
+save(train.corpus,file = "train.corpus.RData")
+
+
+test.corpus <- createCorpus(test.sample)
+rm(test.sample)
+save(test.corpus,file = "test.corpus.RData")
+#dummy.corpus <- createCorpus(c("<s> I am Sam </s>",
+#                               "<s> Sam I am </s>",
+#                               "<s> I do not like green eggs and ham </s>"),clean = F)
 #------------------------------------------------------
 
 
 #Generate N-grams-------------
-unigram.df.blogs <- generateNgramDf(train.corpus.blogs,1)
-bigram.df.blogs <- generateNgramDf(train.corpus.blogs,2)
-trigram.df.blogs <- generateNgramDf(train.corpus.blogs,3)
-tetragram.df.blogs <- generateNgramDf(train.corpus.blogs,4)
+unigram.df <- generateNgramDf(train.corpus,1)
+save(unigram.df,file = "unigram.df.RData")
 
-test.bigram.df.blogs <- generateNgramDf(test.corpus.blogs,2)
-test.trigram.df.blogs <- generateNgramDf(test.corpus.blogs,3)
-test.tetragram.df.blogs <- generateNgramDf(test.corpus.blogs,4)
+bigram.df <- generateNgramDf(train.corpus,2)
+save(bigram.df,file = "bigram.df.RData")
 
-dummy.bigram.df <- generateNgramDf(dummy.corpus,2)
+trigram.df <- generateNgramDf(train.corpus,3)
+save(trigram.df,file = "trigram.df.RData")
 
+tetragram.df <- generateNgramDf(train.corpus,4)
+save(tetragram.df,file = "tetragram.df.RData")
+
+rm(train.corpus)
+
+
+test.unigram.df <- generateNgramDf(test.corpus,1)
+save(test.unigram.df,file = "test.unigram.df.RData")
+
+test.bigram.df <- generateNgramDf(test.corpus,2)
+save(test.bigram.df,file = "test.bigram.df.RData")
+
+test.trigram.df <- generateNgramDf(test.corpus,3)
+save(test.trigram.df,file = "test.trigram.df.RData")
+
+test.tetragram.df <- generateNgramDf(test.corpus,4)
+save(test.tetragram.df,file = "test.tetragram.df.RData")
+
+rm(test.corpus)
+#dummy.bigram.df <- generateNgramDf(dummy.corpus,2)
 #-------------------------------------------------------------------------------------------
 
 #Generate N-gram probabilities---------------------------------------
-unigram.df.blogs <- generateNgramProb(unigram.df.blogs,1)
-bigram.df.blogs <- generateNgramProb(bigram.df.blogs,2)
-trigram.df.blogs <- generateNgramProb(trigram.df.blogs,3)
-tetragram.df.blogs <- generateNgramProb(tetragram.df.blogs,4)
+unigram.df <- generateNgramProb(unigram.df,1)
+save(unigram.df,file = "unigram.df.RData")
 
-dummy.bigram.df <- generateNgramProb(dummy.bigram.df,2)
+bigram.df <- generateNgramProb(bigram.df,2)
+save(bigram.df,file = "bigram.df.RData")
+
+trigram.df <- generateNgramProb(trigram.df,3)
+save(trigram.df,file = "trigram.df.RData")
+
+tetragram.df <- generateNgramProb(tetragram.df,4)
+save(tetragram.df,file = "tetragram.df.RData")
+
+
+#dummy.bigram.df <- generateNgramProb(dummy.bigram.df,2)
 #------------------------------------------------------------------------------
+print("Done!")
 
+#load("unigram.df.RData")
+#load("bigram.df.clean.RData")
+#load("trigram.df.clean.RData")
+#load("tetragram.df.clean.RData")
+#trigram.df.clean <- trigram.df[trigram.df.$Freq > 1,]
 
 #Stupid backoff 3-gram------------------------------------------------------------------------------
 # http://www.aclweb.org/anthology/D07-1090.pdf
 
 ##Parameters
-input <- "I love to live in New york"
-num.results <- 3
+input <- "Go on a romantic date at the"
+num.results <- 30
 alpha = 0.4
 
 #Clean input
 input <- tolower(input) 
 input <- removePunctuation(input)
 input <- removeNumbers(input)
+#input <- removeWords(input, stopwords("english"))
 input <- stripWhitespace(input)
 
 #Get keys
@@ -71,13 +130,16 @@ input.words.size <- length(input.words)
 tetragram.key <- ifelse(input.words.size >= 3, paste(input.words[(input.words.size-2):input.words.size],collapse = " "), NA)
 trigram.key <- ifelse(input.words.size >= 2, paste(input.words[(input.words.size-1):input.words.size],collapse = " "), NA)
 bigram.key <- input.words[input.words.size]
+print(trigram.key)
+print(bigram.key)
 
 #3-gram case, 1.0 (for this 3-gram model) , just the top num.results
-trigram.lines <- grepl(paste0("^",trigram.key,"$"),trigram.df.blogs$key)
-trigram.subset <- trigram.df.blogs[trigram.lines,]
-trigram.subset$score <- trigram.subset$prob * 0.4
+trigram.lines <- grepl(paste0("^",trigram.key,"$"),trigram.df.clean$key)
+sum(trigram.lines)
+trigram.subset <- trigram.df.clean[trigram.lines,]
+trigram.subset$score <- trigram.subset$prob
 trigram.predictions <- head(trigram.subset[order(trigram.subset$score, decreasing = T),],n = num.results)
-
+trigram.predictions
 
 #2-gram case, 0.4 (for this 3-gram model) , just the top num.results
 bigram.lines <- grepl(paste0("^",bigram.key,"$"),bigram.df.blogs$key)
@@ -95,7 +157,55 @@ unigram.predictions <- head(unigram.df.blogs[order(unigram.df.blogs$score, decre
 
 #Stupid backoff 4-gram------------------------------------------------------------------------------
 
+# http://www.aclweb.org/anthology/D07-1090.pdf
 
+##Parameters
+input <- "If this isn't the cutest thing you've ever seen, then you must be"
+num.results <- 30
+alpha = 0.4
+
+#Clean input
+input <- tolower(input) 
+input <- removePunctuation(input)
+input <- removeNumbers(input)
+#input <- removeWords(input, stopwords("english"))
+input <- stripWhitespace(input)
+
+#Get keys
+input.words <- strsplit(input," ")[[1]]
+input.words.size <- length(input.words)
+tetragram.key <- ifelse(input.words.size >= 3, paste(input.words[(input.words.size-2):input.words.size],collapse = " "), NA)
+trigram.key <- ifelse(input.words.size >= 2, paste(input.words[(input.words.size-1):input.words.size],collapse = " "), NA)
+bigram.key <- input.words[input.words.size]
+print(tetragram.key)
+print(trigram.key)
+print(bigram.key)
+
+#4-gram case, 1.0 (for this 4-gram model) , just the top num.results
+tetragram.lines <- grepl(paste0("^",tetragram.key,"$"),tetragram.df.clean$key)
+sum(tetragram.lines)
+tetragram.subset <- tetragram.df.clean[tetragram.lines,]
+tetragram.subset$score <- tetragram.subset$prob
+tetragram.predictions <- head(tetragram.subset[order(tetragram.subset$score, decreasing = T),],n = num.results)
+tetragram.predictions
+
+#3-gram case, 0.4 (for this 4-gram model) , just the top num.results
+trigram.lines <- grepl(paste0("^",trigram.key,"$"),trigram.df.clean$key)
+sum(trigram.lines)
+trigram.subset <- trigram.df.clean[trigram.lines,]
+trigram.subset$score <- trigram.subset$prob * 0.4
+trigram.predictions <- head(trigram.subset[order(trigram.subset$score, decreasing = T),],n = num.results)
+trigram.predictions
+
+#2-gram case, 0.4 * 0.4 (for this 4-gram model) , just the top num.results
+bigram.lines <- grepl(paste0("^",bigram.key,"$"),bigram.df.blogs$key)
+bigram.subset <- bigram.df.blogs[bigram.lines,]
+bigram.subset$score <- bigram.subset$prob * 0.16
+bigram.predictions <- head(bigram.subset[order(bigram.subset$score, decreasing = T),],n = num.results)
+
+#Unigram case, 0.4 * 0.4 * 0.4 (for this 4-gram model) , just the top num.results
+unigram.df.blogs$score <- unigram.df.blogs$prob * 0.064
+unigram.predictions <- head(unigram.df.blogs[order(unigram.df.blogs$score, decreasing = T),],n = num.results)
 
 #------------------------------------------------------------------------------
 
