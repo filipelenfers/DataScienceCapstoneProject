eb655458d82a8e22992673d99e19cc2dec2c40d6
Filipe Pais Lenfers <filipelenfers@gmail.com>
2015-03-29 17:42:25 -0300

    Stupid backoff 4-gram models
    Stupid backoff 4-gram models, with the dataset as is, removing stop
    words and steaming the documento.

--- sp.R
+++ sp.R
@@ -1,13 +1,13 @@
-# Stupid backoff without the stops words
+# Stupid backoff
 
-# predict more meaningfull words
+# some part of the N-grams are dominated by stopwords
 
 library(tm)
 
-load("unigram.df.remstop.RData")
-load("bigram.df.clean.remstop.RData")
-load("trigram.df.clean.remstop.RData")
-load("tetragram.df.clean.remstop.RData")
+load("unigram.df.RData")
+load("bigram.df.clean.RData")
+load("trigram.df.clean.RData")
+load("tetragram.df.clean.RData")
 
 
 #Stupid backoff 4-gram------------------------------------------------------------------------------
@@ -22,7 +22,6 @@ alpha = 0.4
 input <- tolower(input) 
 input <- removePunctuation(input)
 input <- removeNumbers(input)
-input <- removeWords(input, stopwords("english"))
 input <- stripWhitespace(input)
 
 #Get keys
@@ -36,34 +35,34 @@ print(trigram.key)
 print(bigram.key)
 
 #4-gram case, 1.0 (for this 4-gram model) , just the top num.results
-tetragram.lines <- grepl(paste0("^",tetragram.key,"$"),tetragram.df.clean.remstop$key)
+tetragram.lines <- grepl(paste0("^",tetragram.key,"$"),tetragram.df.clean$key)
 sum(tetragram.lines)
-tetragram.subset <- tetragram.df.clean.remstop[tetragram.lines,]
+tetragram.subset <- tetragram.df.clean[tetragram.lines,]
 tetragram.subset$score <- tetragram.subset$prob
 tetragram.predictions <- head(tetragram.subset[order(tetragram.subset$score, decreasing = T),],n = num.results)
 tetragram.predictions
 
 #3-gram case, 0.4 (for this 4-gram model) , just the top num.results
-trigram.lines <- grepl(paste0("^",trigram.key,"$"),trigram.df.clean.remstop$key)
+trigram.lines <- grepl(paste0("^",trigram.key,"$"),trigram.df.clean$key)
 sum(trigram.lines)
-trigram.subset <- trigram.df.clean.remstop[trigram.lines,]
+trigram.subset <- trigram.df.clean[trigram.lines,]
 trigram.subset$score <- trigram.subset$prob * alpha
 trigram.predictions <- head(trigram.subset[order(trigram.subset$score, decreasing = T),],n = num.results)
 trigram.predictions
 
 #2-gram case, 0.4 * 0.4 (for this 4-gram model) , just the top num.results
-bigram.lines <- grepl(paste0("^",bigram.key,"$"),bigram.df.clean.remstop$key)
-bigram.subset <- bigram.df.clean.remstop[bigram.lines,]
+bigram.lines <- grepl(paste0("^",bigram.key,"$"),bigram.df.clean$key)
+bigram.subset <- bigram.df.clean[bigram.lines,]
 bigram.subset$score <- bigram.subset$prob * (alpha ^ 2)
 bigram.predictions <- head(bigram.subset[order(bigram.subset$score, decreasing = T),],n = num.results)
 bigram.predictions
 
 #Unigram case, 0.4 * 0.4 * 0.4 (for this 4-gram model) , just the top num.results
-unigram.df.remstop$score <- unigram.df.remstop$prob * (alpha ^ 3)
-unigram.predictions <- head(unigram.df.remstop[order(unigram.df.remstop$score, decreasing = T),],n = num.results)
+unigram.df$score <- unigram.df$prob * (alpha ^ 3)
+unigram.predictions <- head(unigram.df[order(unigram.df$score, decreasing = T),],n = num.results)
 unigram.predictions
 
-spStopwordPredictions <- rbind(tetragram.predictions,trigram.predictions,bigram.predictions,unigram.predictions)
+spPredictions <- rbind(tetragram.predictions,trigram.predictions,bigram.predictions,unigram.predictions)
 
 
 #------------------------------------------------------------------------------
