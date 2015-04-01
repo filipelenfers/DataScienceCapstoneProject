820621b44f9862aaf28b0dc60c69fa253ff9aa55
Filipe Pais Lenfers <filipelenfers@gmail.com>
2015-04-01 20:23:43 -0300

    Moved creatios of the test data to a file.

--- sp.R
+++ sp.R
@@ -68,39 +68,6 @@ spPredictions <- rbind(tetragram.predictions,trigram.predictions,bigram.predicti
 #------------------------------------------------------------------------------
 
 #Accuracy check----------------------------------------------------------------
-load("test.sample.RData")
 
-library(doParallel)
-library(tm)
-
-nodes <- detectCores() - 1
-cl <- makeCluster(nodes)
-registerDoParallel(cl)
-
-phraseToTestPattern <- function(phrase) {
-  words <- unlist(strsplit(phrase," "))
-  target <- removePunctuation(words[length(words)])
-  input <- paste(words[1:length(words)-1], collapse = " ")
-  c(input,target)
-}
-
-
-#For each item, everything as input, and las word as taget.
-parallelFunction <- function() {
-  system.time(outForeach <- foreach(i=1:length(test.sample),.combine = rbind, .inorder = F, .export = c("removePunctuation"), .multicombine = T  ) %dopar% {
-    phraseToTestPattern(test.sample[i])  
-  })
-}
-#Sapply version
-system.time({
-  outSapply <- sapply(test.sample,phraseToTestPattern,USE.NAMES = F) 
-  outSapply <- t(outSapply)
-}  )
-
-
-stopCluster(cl)
-
-save(outSapply,file = "outSapply.RData")
-save(outForeach,file = "outForeach.RData")
 
 #------------------------------------------------------------------------------
